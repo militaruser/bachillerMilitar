dc992f59e9def8effa97745415f3368a245b6c69
BLACKDEATH <blackdeath166@gmail.com>
2023-11-12 12:19:03 -0600

    configuracion firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,21 @@
 export const environment =
   {
-    firebaseConfig: {
-      apiKey: 'AIzaSyC-GcLYtf9lA0ySlxYiuQUWZi782ifM2Nc',
-      authDomain: 'siscomapx-dev.firebaseapp.com',
-      projectId: 'siscomapx-dev',
-      storageBucket: 'siscomapx-dev.appspot.com',
-      messagingSenderId: '496873279991',
-      appId: '1:496873279991:web:29b95e3951321f60eadef2'
+    // firebaseConfig: {
+    //   apiKey: 'AIzaSyC-GcLYtf9lA0ySlxYiuQUWZi782ifM2Nc',
+    //   authDomain: 'siscomapx-dev.firebaseapp.com',
+    //   projectId: 'siscomapx-dev',
+    //   storageBucket: 'siscomapx-dev.appspot.com',
+    //   messagingSenderId: '496873279991',
+    //   appId: '1:496873279991:web:29b95e3951321f60eadef2'
+    // },
+        firebaseConfig: {
+      apiKey: "AIzaSyCzyzZdcViGGDjw2T71Y1cXTXqaC8xayZg",
+      authDomain: "bachillerato-militarizado.firebaseapp.com",
+      projectId: "bachillerato-militarizado",
+      storageBucket: "bachillerato-militarizado.appspot.com",
+      messagingSenderId: "809728536957",
+      appId: "1:809728536957:web:9326a081ff8544ed4d5c14",
+      measurementId: "G-WYQKB9JRF1"
     },
     production: false,
     apiGraphql: "http://localhost:3333/graphql",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,22 @@
 export const environment =
   {
-    // firebaseConfig: {
-    //   apiKey: "AIzaSyCzyzZdcViGGDjw2T71Y1cXTXqaC8xayZg",
-    //   authDomain: "bachillerato-militarizado.firebaseapp.com",
-    //   projectId: "bachillerato-militarizado",
-    //   storageBucket: "bachillerato-militarizado.appspot.com",
-    //   messagingSenderId: "809728536957",
-    //   appId: "1:809728536957:web:9326a081ff8544ed4d5c14",
-    //   measurementId: "G-WYQKB9JRF1"
-    // },
     firebaseConfig: {
-      apiKey: 'AIzaSyC-GcLYtf9lA0ySlxYiuQUWZi782ifM2Nc',
-      authDomain: 'siscomapx-dev.firebaseapp.com',
-      projectId: 'siscomapx-dev',
-      storageBucket: 'siscomapx-dev.appspot.com',
-      messagingSenderId: '496873279991',
-      appId: '1:496873279991:web:29b95e3951321f60eadef2'
+      apiKey: "AIzaSyCzyzZdcViGGDjw2T71Y1cXTXqaC8xayZg",
+      authDomain: "bachillerato-militarizado.firebaseapp.com",
+      projectId: "bachillerato-militarizado",
+      storageBucket: "bachillerato-militarizado.appspot.com",
+      messagingSenderId: "809728536957",
+      appId: "1:809728536957:web:9326a081ff8544ed4d5c14",
+      measurementId: "G-WYQKB9JRF1"
     },
+    // firebaseConfig: {
+    //   apiKey: 'AIzaSyC-GcLYtf9lA0ySlxYiuQUWZi782ifM2Nc',
+    //   authDomain: 'siscomapx-dev.firebaseapp.com',
+    //   projectId: 'siscomapx-dev',
+    //   storageBucket: 'siscomapx-dev.appspot.com',
+    //   messagingSenderId: '496873279991',
+    //   appId: '1:496873279991:web:29b95e3951321f60eadef2'
+    // },
     production: true,
     apiGraphql: "http://localhost:3333/graphql",
     wsGraphql: "ws://localhost:3333/graphql"
